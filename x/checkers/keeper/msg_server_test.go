57a8e11d554573fccd84476670df69bd6b78910a
root <root@DESKTOP-HC1H81C>
2023-11-12 20:41:30 +0300

    Initial Commit

---
+++ x/checkers/keeper/msg_server_test.go
@@ -0,0 +1,23 @@
+package keeper_test
+
+import (
+	"context"
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+    "github.com/okemwa/checkers/x/checkers/types"
+    "github.com/okemwa/checkers/x/checkers/keeper"
+    keepertest "github.com/okemwa/checkers/testutil/keeper"
+)
+
+func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
+	k, ctx := keepertest.CheckersKeeper(t)
+	return keeper.NewMsgServerImpl(*k), sdk.WrapSDKContext(ctx)
+}
+
+func TestMsgServer(t *testing.T) {
+	ms, ctx := setupMsgServer(t)
+	require.NotNil(t, ms)
+	require.NotNil(t, ctx)
+}
---
+++ x/checkers/keeper/query.go
@@ -0,0 +1,7 @@
+package keeper
+
+import (
+	"github.com/okemwa/checkers/x/checkers/types"
+)
+
+var _ types.QueryServer = Keeper{}
